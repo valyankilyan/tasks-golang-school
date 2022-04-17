0f960920432452a2e5b1a7f5dcb6a59ea71eba01
Sergey 'Sirius' Ivliev <sivliev@ozon.ru>
2022-04-17 10:23:00 +0300

    fix

--- checker.go
+++ checker.go
@@ -40,8 +40,7 @@ func main() {
 	dirs := make([]string, 0, len(files))
 	for _, f := range files {
 		if f.IsDir() {
-			log.Printf("%v", f.Name())
-			if _, err := os.Stat(f.Name() + "/README.MD"); err == nil {
+			if _, err := os.Stat(f.Name() + "/README.md"); err == nil {
 				dirs = append(dirs, f.Name())
 			}
 		}
