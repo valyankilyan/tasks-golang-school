2369179cc1964725deaf080eca901aed4611efc9
Валентин Киль <valentin.kil.2002@gmail.com>
2022-04-24 03:47:22 +0300

    coins done but uneffective (i suppose)

--- coins/coins.go
+++ coins/coins.go
@@ -1,5 +1,20 @@
 package coins
 
+func Rec(left, mx int) int {
+	if left == 0 {
+		return 1
+	}
+	ans := 0
+	for i := mx; i <= left; i++ {
+		ans += Rec(left-i, i)
+	}
+	return ans
+}
+
 func Piles(n int) int {
-	return (1 << (n - 1)) - 1
+	ans := 0
+	for i := 1; i <= n; i++ {
+		ans += Rec(n-i, i)
+	}
+	return ans
 }
