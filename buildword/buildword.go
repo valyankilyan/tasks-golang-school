205dcc162b43d8dfd80ebf171235192a9a508601
Валентин Киль <valentin.kil.2002@gmail.com>
2022-04-24 03:20:18 +0300

    buildword is done

--- buildword/buildword.go
+++ buildword/buildword.go
@@ -1,5 +1,42 @@
 package buildword
 
+const INF int = 100000009
+
+func Match(word, fragment string) int {
+	if len(word) < len(fragment) {
+		return 0
+	}
+	for i := range fragment {
+		if word[i] != fragment[i] {
+			return 0
+		}
+	}
+	return len(fragment)
+}
+
+func Rec(word string, fragments []string) int {
+	mn := INF
+	if len(word) == 0 {
+		return 0
+	}
+	for _, fragment := range fragments {
+		if l := Match(word, fragment); l != 0 {
+			if ans := Rec(word[l:], fragments); mn > ans {
+				mn = ans
+			}
+		}
+	}
+	if mn != INF {
+		mn++
+	}
+	return mn
+}
+
 func BuildWord(word string, fragments []string) int {
-	return 0
+	ans := Rec(word, fragments)
+	if ans == INF {
+		return 0
+	} else {
+		return ans
+	}
 }
