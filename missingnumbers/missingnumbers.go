200da393d3ddd56ac080eceb354fa3ac28c8d38f
Валентин Киль <valentin.kil.2002@gmail.com>
2022-04-24 15:28:38 +0300

    missingnumbers

--- missingnumbers/missingnumbers.go
+++ missingnumbers/missingnumbers.go
@@ -1,5 +1,15 @@
 package missingnumbers
 
 func Missing(numbers []int) []int {
-	return []int{}
+	exist := make([]bool, len(numbers)+2)
+	for _, n := range numbers {
+		exist[n-1] = true
+	}
+	var ans []int
+	for num, e := range exist {
+		if !e {
+			ans = append(ans, num+1)
+		}
+	}
+	return ans
 }
